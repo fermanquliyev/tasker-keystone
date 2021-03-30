3a75534f4577d7d23f24fcad2d5954b3085faedc
fermanquliyev <33841624+fermanquliyev@users.noreply.github.com>
2021-03-30 21:12:35 +0400

    Upgrade packages and remove babel presets causing error

--- schemas/ProviderWorkArea.ts
+++ schemas/ProviderWorkArea.ts
@@ -6,8 +6,8 @@ export const ProviderWorkArea = list({
         title: Types.text(),
         city: Types.text({isRequired:true}),
         country: Types.text({isRequired:true}),
-        lat: Types.text({isRequired:true, isIndexed:true}), // can't find float type
-        long: Types.text({isRequired:true, isIndexed:true}),
+        lat: Types.float({isRequired:true, isIndexed:true}),
+        long: Types.float({isRequired:true, isIndexed:true}),
         schedule:Types.relationship({
             ref:'ProviderWorkSchedule',
             many:false,
